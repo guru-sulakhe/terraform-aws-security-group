98858cd3a334644b3f07f46a969ba379d8e664d0
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-16 13:20:57 +0530

    adding note to understand module development

--- main.tf
+++ main.tf
@@ -21,3 +21,4 @@ resource "aws_security_group" "main" {
 
 #Note: 
     #During module development the variable would be included empty,which means the user has to supply the values to the variables which are empty in the module development
+    #provider.tf must be indluded in during the module usage process
