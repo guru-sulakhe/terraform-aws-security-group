6df8dc5ab155f09bbbe785042dc856037abee93d
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-03-20 11:43:55 +0530

    comment line

--- main.tf
+++ main.tf
@@ -20,5 +20,5 @@ resource "aws_security_group" "main" {
 }
 
 #Note: 
-    #During module development the variable would be included empty,which means the user has to supply the values to the variables which are empty in the module development
+    #During module development the variable would be included empty,which means the user has to supply the values to the variables which are empty in the module development in the process of module usage
     #provider.tf must be indluded in during the module usage process
